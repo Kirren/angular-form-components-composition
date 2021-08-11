2973ef23a7e72a2bfe4149dc5f679a4cbdd0756a
Kirren <kirren@live.com>
2021-08-11 17:32:23 +0300

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,62 +36,61 @@ export class AppComponent {
   control: FormControl;
   selection: SelectionModel<Item>;
 
+  @ViewChild('fruitInput') inputElement: HTMLInputElement;
+
   constructor() {
     this.control = new FormControl([this.options[0]]);
   }
 
   ngOnInit(): void {
-    console.log(this.control.value);
     this.selection = new SelectionModel(true, this.control.value, true);
 
-    console.log(this.selection.selected);
-
-    // this.valueChangeSubscription = this.control.valueChanges.subscribe(
-    //   (value) => {
-    //     this._filter(value);
-    //   }
-    // );
+    this.valueChangeSubscription = this.control.valueChanges.subscribe(
+      (value) => {
+        this._filter(value);
+      }
+    );
   }
 
-  // public filteredOptions: Item[] = this.options;
-  // public filteredMatch: Item = null;
+  public filteredOptions: Item[] = this.options;
+  public filteredMatch: Item = null;
 
-  // valueChangeSubscription: Subscription;
+  valueChangeSubscription: Subscription;
 
-  // ngOnDestroy() {
-  //   this.valueChangeSubscription.unsubscribe();
-  // }
+  ngOnDestroy() {
+    this.valueChangeSubscription.unsubscribe();
+  }
 
-  // private _filter(value: string): void {
-  //   if (value && typeof value === 'string') {
-  //     const keyMatch = this.options?.find(
-  //       (option) => option.key?.toLowerCase() === value.toLowerCase()
-  //     );
-  //     if (keyMatch) {
-  //       this.filteredOptions = [keyMatch];
-  //     } else {
-  //       this.filteredOptions = this.options.filter((option) => {
-  //         const text =
-  //           (option.name || '') +
-  //           (option.key || '');
-  //         return text.toLowerCase().indexOf(value.toLowerCase()) >= 0;
-  //       });
-  //     }
-  //     this.filteredMatch =
-  //       this.filteredOptions?.length === 1 ? this.filteredOptions[0] : null;
-  //   } else {
-  //     this.filteredOptions = this.options;
-  //     this.filteredMatch = null;
-  //   }
-  // }
+  private _filter(value: string): void {
+    if (value && typeof value === 'string') {
+      const keyMatch = this.options?.find(
+        (option) => option.key?.toLowerCase() === value.toLowerCase()
+      );
+      if (keyMatch) {
+        this.filteredOptions = [keyMatch];
+      } else {
+        this.filteredOptions = this.options.filter((option) => {
+          const text =
+            (option.name || '') +
+            (option.key || '');
+          return text.toLowerCase().indexOf(value.toLowerCase()) >= 0;
+        });
+      }
+      this.filteredMatch =
+        this.filteredOptions?.length === 1 ? this.filteredOptions[0] : null;
+    } else {
+      this.filteredOptions = this.options;
+      this.filteredMatch = null;
+    }
+  }
 
-  // public onOptionSelected(event: MatAutocompleteSelectedEvent) {
-  //   const value = event.option.value;
+  public onOptionSelected(event: MatAutocompleteSelectedEvent) {
+    const value = event.option.value;
 
-  //   this.selection.toggle(value);
-  // }
+    this.selection.toggle(value);
+  }
 
-  // public displayFn(option: Item): string {
-  //   return option && option.name ? option.name : null;
-  // }
+  public displayFn(option: Item): string {
+    return option && option.name ? option.name : null;
+  }
 }
--- src/app/form-field/form-field.component.ts
+++ src/app/form-field/form-field.component.ts
@@ -1,4 +1,10 @@
-import { Component, ContentChild, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ContentChild,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import {
   MatFormField,
   MatFormFieldControl
@@ -9,13 +15,17 @@ import { Item } from '../app.component';
   selector: 'app-form-field',
   templateUrl: './form-field.component.html'
 })
-export class FormFieldComponent implements OnInit {
+export class FormFieldComponent implements OnInit, AfterViewInit {
   @ContentChild(MatFormFieldControl, { static: true })
   public formFieldControl: MatFormFieldControl<Item>;
   @ViewChild(MatFormField, { static: true })
   public matFormField: MatFormField;
 
   ngOnInit(): void {
+    this.matFormField._control = this.formFieldControl;
+  }
+
+  ngAfterViewInit() {
     this.matFormField._control = this.formFieldControl;
   }
 }
